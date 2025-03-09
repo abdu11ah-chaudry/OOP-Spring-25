1745b5340e37d6e48ac483f42ac0b70f0c5a9d1c
abdu11ah-chaudry <abdchaudry1205@gmail.com>
2025-03-09 16:01:26 +0500

    Update task01.cpp

--- Lab 06/task01.cpp
+++ Lab 06/task01.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
 
+class Employee{
+protected:
+    string name;
+    float salary;
+public:
+    Employee(string n, float sal):name(n),salary(sal){}
+
+    void display(){
+        cout<<"Employee Name: "<<name<<endl;
+        cout<<"Employee Salary: "<<salary<<endl;
+    }
+};
+
+class Manager:private Employee{
+private:
+    float bonus;
+public:
+    Manager(string n, float sal, float b):Employee(n,sal),bonus(b){}
+    
+    void display(){
+        Employee::display();
+        cout<<"Bonus: "<<bonus<<endl;
+    }
+};
+
+int main(){
+
+    Manager m("Abdullah",100000,1000);
+    m.display();
+
+    return 0;
+}
